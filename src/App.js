70f72262f42c208bdc279ac722f8569adee75f1a
SalahQj <qj.salaheddine@gmail.com>
2025-05-22 12:28:41 +0100

    commit de salah

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './App.css';
-import carImage from './car.jpg'; // Assure-toi d'avoir l'image car.jpg dans le dossier src
+import carImage from './car.jpg'; // La même image pour les deux
 
 function App() {
   return (
     <div className="container">
       <header className="header">
-        <h1>Bienvenue sur la page de voiture 2025 </h1>
+        <h1>Bienvenue sur la page de voiture 2025</h1>
       </header>
 
       <main className="main">
-        <img src={carImage} alt="Voiture" className="car-image" />
+        <img src={carImage} alt="Voiture 1" className="car-image" />
+        <img src={carImage} alt="Voiture 2" className="car-image" />
         <p>Mercedes-AMG C 63 S E PERFORMANCE.</p>
       </main>
 
